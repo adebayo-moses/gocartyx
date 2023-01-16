3125a591510865f1160b9f570129ccba436256e5
Moses Adebayo <adebayomoses597@gmail.com>
2023-01-16 21:02:52 +0100

    build: review section component

---
+++ src/components/Reviews.vue
@@ -0,0 +1,203 @@
+<template>
+  <div>
+    <section class="nd pt" data-aos-id-6="">
+      <div class="x ie tf">
+        <!-- Bg -->
+        <div class="w _ sw nc hj oy ok m ts" aria-hidden="true"></div>
+
+        <div class="rw tf up hd">
+          <div class="uv dr">
+            <!-- Section content -->
+            <div class="x rj tf ph aa dh rt iv pk ib iw">
+              <!-- Content -->
+              <div class="rd rx ir hw">
+                <!-- Copy -->
+                <h2
+                  class="h2 nh"
+                  data-aos="fade-up"
+                  data-aos-anchor="[data-aos-id-6]"
+                  data-aos-delay="100"
+                >
+                  Compliance and Regulated with the Financial Sector
+                </h2>
+                <p
+                  class="ap fe ns"
+                  data-aos="fade-up"
+                  data-aos-anchor="[data-aos-id-6]"
+                  data-aos-delay="200"
+                >
+                  We register our platform with the regulatory bodies to ensure
+                  safe bon and trust with our clients and risk of loss of
+                  assets.
+                </p>
+                <ul
+                  class="rn iv fe iz"
+                  data-aos="fade-up"
+                  data-aos-anchor="[data-aos-id-6]"
+                  data-aos-delay="300"
+                >
+                  <li class="rt ib">
+                    <svg
+                      class="ir tb"
+                      width="20"
+                      height="20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle class="ui" cx="10" cy="10" r="10"></circle>
+                      <path
+                        class="un"
+                        d="M15.335 7.933 14.87 7c-4.025 1.167-6.067 3.733-6.067 3.733l-1.867-1.4-.933.934L8.802 14c2.158-4.025 6.533-6.067 6.533-6.067Z"
+                      ></path>
+                    </svg>
+                    <span>Secure Assets</span>
+                  </li>
+                  <li class="rt ib">
+                    <svg
+                      class="ir tb"
+                      width="20"
+                      height="20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle class="ui" cx="10" cy="10" r="10"></circle>
+                      <path
+                        class="un"
+                        d="M15.335 7.933 14.87 7c-4.025 1.167-6.067 3.733-6.067 3.733l-1.867-1.4-.933.934L8.802 14c2.158-4.025 6.533-6.067 6.533-6.067Z"
+                      ></path>
+                    </svg>
+                    <span>No risk of loss.</span>
+                  </li>
+                  <li class="rt ib">
+                    <svg
+                      class="ir tb"
+                      width="20"
+                      height="20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle class="ui" cx="10" cy="10" r="10"></circle>
+                      <path
+                        class="un"
+                        d="M15.335 7.933 14.87 7c-4.025 1.167-6.067 3.733-6.067 3.733l-1.867-1.4-.933.934L8.802 14c2.158-4.025 6.533-6.067 6.533-6.067Z"
+                      ></path>
+                    </svg>
+                    <span>Personal Informations Secured</span>
+                  </li>
+                  <li class="rt ib">
+                    <svg
+                      class="ir tb"
+                      width="20"
+                      height="20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle class="ui" cx="10" cy="10" r="10"></circle>
+                      <path
+                        class="un"
+                        d="M15.335 7.933 14.87 7c-4.025 1.167-6.067 3.733-6.067 3.733l-1.867-1.4-.933.934L8.802 14c2.158-4.025 6.533-6.067 6.533-6.067Z"
+                      ></path>
+                    </svg>
+                    <span>Global regulations and compliance</span>
+                  </li>
+                </ul>
+              </div>
+
+              <!-- Carousel -->
+              <div
+                class="rp rz ph pn nd hz"
+                data-aos="fade-up"
+                data-aos-anchor="[data-aos-id-6]"
+              >
+                <div class="x rz tf">
+                  <div class="w _ nm ng tp oc ts" aria-hidden="true"></div>
+
+                  <!-- Carousel built with Swiper.js [https://swiperjs.com/] -->
+                  <!-- * Initialized in the scripttagin index.html -->
+                  <div class="testimonial-carousel swiper-container rz tf hu">
+                    <div class="swiper-wrapper">
+                      <!-- Card #1 -->
+                      <div class="swiper-slide rt iv ro fj au">
+                        <div class="x lg ly ce ct">
+                          <img
+                            src="../assets/quote.jpeg"
+                            class="ph"
+                            width="384"
+                            height="180"
+                            alt="Testiomonial 01"
+                          />
+                        </div>
+                        <div class="ii rt iv x ob oj ue uc an">
+                          <img
+                            class="w z iu"
+                            src="../assets/testimonial-sign-01.svg"
+                            width="150"
+                            height="71"
+                            alt="Testiomonial sign 01"
+                          />
+                          <p class="ii aw ff nh">
+                            “ We are building the future of digital assets
+                            exchange here at GoCartyX, making digital assets
+                            exchange very easy and feasible while maintaining
+                            the best user experience. “
+                          </p>
+                          <div class="aw ah fe">
+                            <span class="ff">Muiz Adeyemil</span> -
+                            <span class="ft">CEO &amp; Co-Founder</span>
+                          </div>
+                        </div>
+                      </div>
+
+                      <!-- Card #2 -->
+                      <div class="swiper-slide rt iv ro fj au">
+                        <div class="x lg ly ce ct">
+                          <img
+                            src="../assets/quote.jpeg"
+                            class="ph"
+                            width="384"
+                            height="180"
+                            alt="Testiomonial 02"
+                          />
+                        </div>
+                        <div class="ii rt iv x ob oj ue uc an">
+                          <img
+                            class="w z iu"
+                            src="../assets/testimonial-sign-01.svg"
+                            width="105"
+                            height="46"
+                            alt="Testiomonial sign 02"
+                          />
+                          <p class="ii aw ff nh">
+                            “ We are building the future of digital assets
+                            exchange here at GoCartyX, making digital assets
+                            exchange very easy and feasible while maintaining
+                            the best user experience. “
+                          </p>
+                          <div class="aw ah fe">
+                            <span class="ff">Moses Max</span> -
+                            <span class="ft">Engineer &amp; CTO</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- Bullets -->
+                  <div class="ny">
+                    <div class="testimonial-carousel-pagination aa"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
